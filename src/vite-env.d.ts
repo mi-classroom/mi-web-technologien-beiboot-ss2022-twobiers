5549fa6bc77e15327bafa0f9c4d7625456b1cfa5
twobiers <22715034+twobiers@users.noreply.github.com>
2022-04-26 17:31:06 +0200

    Add conditional base path for GitHub Pages

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImprotMetaEnv {
+    readonly VITE_BASE_URL: string | undefined;
+}
+
+interface ImportMeta {
+    readonly env: ImportMeta;
+}
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ command, mode}) => {
+    const env = loadEnv(mode, process.cwd());
+    return {
+        base: env.VITE_BASE_URL || '/'
+    }
+});
