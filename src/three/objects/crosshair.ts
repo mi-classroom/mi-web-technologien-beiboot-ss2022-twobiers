be0bff6866cd59c560b1eca3205151ad8b346992
twobiers <22715034+twobiers@users.noreply.github.com>
2022-05-30 17:09:24 +0200

    move crosshair in separate file

---
+++ src/three/objects/crosshair.ts
@@ -0,0 +1,17 @@
+import * as THREE from "three";
+
+const cursorSize = 0.75;
+const cursorThickness = 1.15;
+const cameraMin = 0.1;
+const cursorGeometry = new THREE.RingBufferGeometry(
+    cursorSize * cameraMin,
+    cursorSize * cameraMin * cursorThickness,
+    32,
+    0,
+    Math.PI * 0.5,
+    Math.PI * 2);
+const cursorMaterial = new THREE.MeshBasicMaterial({ color: "white" });
+const cursor = new THREE.Mesh(cursorGeometry, cursorMaterial);
+cursor.position.z = -cameraMin * 50;
+
+export const Crosshair = cursor;
