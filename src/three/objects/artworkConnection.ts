8d744216fbd06a806005c6a4bc9148c52b1f641a
twobiers <22715034+twobiers@users.noreply.github.com>
2022-06-14 22:09:09 +0200

    reduce tube size

--- src/three/objects/artworkConnection.ts
+++ src/three/objects/artworkConnection.ts
@@ -9,9 +9,12 @@ export class ArtworkConnection extends Mesh {
         const targetPoint = new Vector3();
         source.getWorldPosition(sourcePoint);
         target.getWorldPosition(targetPoint);
+        // sourcePoint.y -= (source.height / 2) * 10;
+        // targetPoint.y -= (target.height) / 2 * 10;
+
         const curve = new LineCurve3(sourcePoint, targetPoint);
 
-        const geometry = new TubeGeometry( curve, 100, 0.5, 8, false );
+        const geometry = new TubeGeometry( curve, 100, 0.1, 8, false );
         const material = new MeshBasicMaterial( { color: 0x00ff00 } );
         super(geometry, material);
     }
